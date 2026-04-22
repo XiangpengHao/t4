83edaf77e14be602eeb68be22931eeac5c3aaf71
Devan <devandbenz@gmail.com>
2026-04-22 17:20:35 -0500

    feat: Working on gating io_uring

--- src/io/mod.rs
+++ src/io/mod.rs
@@ -1,5 +1,8 @@
 mod common;
 pub(crate) mod error;
 pub(crate) mod io_task;
-pub(crate) mod io_uring;
 pub(crate) mod sync;
+
+#[cfg(feature = "io-uring")]
+#[cfg(target_os = "linux")]
+pub(crate) mod io_uring;
